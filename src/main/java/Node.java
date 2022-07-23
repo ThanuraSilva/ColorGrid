b336d9c13262d5850aaf0b91e79929fd032a69d7
ThanuraSilva <thanurasilva1989@gmail.com>
2022-07-23 13:22:55 +0530

    Create Node

---
+++ src/main/java/Node.java
@@ -0,0 +1,37 @@
+public class Node {
+    private String color;
+    private int x;
+    private int y;
+
+    public Node() {}
+
+    public Node(String color, int x, int y) {
+        this.color = color;
+        this.x = x;
+        this.y = y;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
